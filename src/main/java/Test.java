2f7774bdb784bf6e83592a376753457bcab02c3a
caotinghui <292012944@qq.com>
2021-09-23 20:42:08 +0800

    push

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(3);
+        Random random = new Random();
+        int i = random.nextInt(3);
+        System.out.println(i);
+    }
+}
